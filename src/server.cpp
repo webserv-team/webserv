d94d9b1e20681db9266b08ea3e969c52d8ea490e
emohamedd <a.elgaouej@gmail.com>
2024-02-04 15:13:11 +0100

    the server from 0 :_) : because now each client have his own socket ; adding the struct client

--- src/server.cpp
+++ src/server.cpp
@@ -6,7 +6,7 @@
 /*   By: emohamed <emohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 11:26:17 by emohamed          #+#    #+#             */
-/*   Updated: 2024/02/01 13:19:14 by emohamed         ###   ########.fr       */
+/*   Updated: 2024/02/04 15:12:14 by emohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,18 @@
 #include "../parsing/ServerConf.hpp"
 #include "../parsing/Request.hpp"
 struct sockaddr_in address;
-#define BUFFER_SIZE 10000
+#define BUFFER_SIZE 1024
 
 #define GREEN "\033[1;32m"
 #define RED "\033[1;31m"
 #define YELLOW "\033[1;33m"
 #define RESET "\033[0m"
+
+struct client{
+	int socket;
+	std::string request;
+	std::string response;
+};
 
 std::string getContentType(const std::string& filePath)
 {
@@ -131,7 +137,7 @@ void sendResponse(int socket, Request& request, ConfigData& server)
 	else
 		filePath += request.getURL();
 
-    std::cout <<  "---->   " << filePath << std::endl;
+    // std::cout <<  "---->   " << filePath << std::endl;
 	std::ifstream file(filePath);
 	if (file.is_open())
 	{
@@ -142,12 +148,14 @@ void sendResponse(int socket, Request& request, ConfigData& server)
 		
 		std::string response = "HTTP/1.1 200 OK\r\nContent-Type: " + getContentType(filePath) + "\r\nContent-Length: ";
 		response += std::to_string(content.length()) + "\r\n\r\n" + content;
-		if (send(socket, response.c_str(), response.length(), 0) < 0)
+		int sizes = 0;
+		if ((sizes = send(socket, response.c_str(), response.length(), 0)) < 0)
 		{
 			std::cerr << RED << "Send error: " << strerror(errno) << RESET << std::endl;
 			error500(socket, server.errorPages["server_error"]);
 			return;
 		}	
+		std::cout << RED << "size sended : " << sizes << RESET <<  " | " <<  YELLOW << "full size: "<<response.length()  << RESET<< std::endl;
 		std::cout << GREEN << "Response sent : " << RESET << RED << request.getURL() << RESET << std::endl;
 	}
     else
@@ -175,175 +183,117 @@ void sendResponse(int socket, Request& request, ConfigData& server)
 	}
 }
 
-int main(){
-    ServerConf data("../default.toml");
-    int port = data.getServers()[0].port;
-    int new_socket;
-    int server = socket(AF_INET, SOCK_STREAM, 0);
-    if(server < 0){
-        std::cerr << "Socket creation error" << std::endl;
-        exit(1);
-    }
-    int opt = 1;
+
+int main(int ac, char **av){
+	if(ac != 2){
+		std::cerr << "Usage: ./server <config_file>" << std::endl;
+		return 1;
+	}
+	ServerConf data(av[1]);
+	int port = data.getServers()[0].port;
+	int new_socket;
+	int server = socket(AF_INET, SOCK_STREAM, 0);
+	if (server < 0)
+	{
+		std::cerr  << RED << "Socket creation error: " << strerror(errno) << RESET << std::endl;
+		return 1;
+	}
+	 int opt = 1;
     if(setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1){
         std::cerr << "Setsockopt eroor: " << strerror(errno) << std::endl;
         return -1;
     }
-    memset((char *)&address, 0, sizeof(address));
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = htonl(INADDR_ANY);
-    address.sin_port = htons(port);
-    int b = bind(server, (struct sockaddr *)&address, sizeof(address));
-     if(b < 0){
-        std::cerr << "Bind failed" << std::endl;
-        exit(1);
-    }
-    int l = listen(server, 10);
-	if(l < 0){
-		std::cerr << "Listen failed" << std::endl;
-		exit(1);
+	memset(&address, 0, sizeof(address));
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(port);
+	if (bind(server, (struct sockaddr *)&address, sizeof(address)) < 0)
+	{
+		std::cerr << RED << "Bind error: " << strerror(errno) << RESET << std::endl;
+		return 1;
 	}
-    socklen_t addrlen = sizeof(address);
-    std::vector<ConfigData> servers = data.getServers();
+	if (listen(server, 3) < 0)
+	{
+		std::cerr << RED << "Listen error: " << strerror(errno) << RESET << std::endl;
+		return 1;
+	}
+	socklen_t addrlen = sizeof(address);
+	std::vector<client> clients;
+	std::vector<ConfigData> servers = data.getServers();
 	signal(SIGPIPE, SIG_IGN);
-    char buff[BUFFER_SIZE];
-	fd_set master; // master file descriptor set
-	FD_ZERO(&master); // clear the master set
-	FD_SET(server, &master); // add the listener to the master set
-// 	while(true){
+	char buffer[BUFFER_SIZE] ;
+	fd_set master;
+	FD_ZERO(&master);
+	FD_SET(server, &master);
+	while(1){
+		std::cout << GREEN << "Waiting for connections..." << RESET << std::endl;
+		fd_set copy = master;
+		
+		if(select(FD_SETSIZE, &copy, nullptr, nullptr, nullptr) < 0){
+			std::cerr << RED << "Select error: " << strerror(errno) << RESET << std::endl;
+			return 1;
+		}
+		for(int i = 0; i < FD_SETSIZE; i++){
+			if(FD_ISSET(i , &copy)){
+				int sock = i;
+				if(sock == server){
+					new_socket = accept(server, (struct sockaddr *)&address, &addrlen);
+					if (new_socket < 0)
+					{
+						std::cerr << RED << "Accept error: " << strerror(errno) << RESET << std::endl;
+						return 1;
+					}
+					int flags = fcntl(new_socket, F_GETFL, 0);
+					if(flags < 0){
+						std::cerr << RED << "Fcntl error: " << strerror(errno) << RESET << std::endl;
+						return 1;
+					}
+					flags |= O_NONBLOCK;
+					if(fcntl(new_socket, F_SETFL, flags) < 0){
+						std::cerr << RED << "Fcntl error: " << strerror(errno) << RESET << std::endl;
+						return 1;
+					}
+					FD_SET(new_socket, &master);
+					client newClient;
+					newClient.socket = new_socket;
+					clients.push_back(newClient);
+					
+				}
+				else{
+					client newClient;
+					for(std::vector<client>::iterator it = clients.begin(); it != clients.end(); it++){
+						if(it->socket == sock){
+							newClient = *it;
+							break;
+						}
+					
+					}
+					int r = recv(newClient.socket, buffer, BUFFER_SIZE, 0);
+					if(r < 0){
+						std::cerr << RED << "Recv error: " << strerror(errno) << RESET << std::endl;
+						
+					}
+					// buffer[r] = '\0';
+					newClient.request += buffer;
 
-//     // Make a copy of the master file descriptor set, because select() modifies the set
-//     fd_set copy = master;
-//     // Call select() to wait for activity on any of the sockets in the set
-//     // The first parameter is ignored in modern implementations, but traditionally it's the highest-numbered file descriptor in any of the sets, plus one
-//     // The second parameter is the set of file descriptors to monitor for read readiness
-//     // The last three parameters are sets of file descriptors to monitor for write readiness, error conditions, and timeout, all of which we're not using
-//     // int socketCount = select(server+1, &copy, nullptr, nullptr, nullptr);
-
-//     // // Check if select() returned an error
-//     // if(socketCount == -1){
-//     //     std::cerr << "select error: " << strerror(errno) << std::endl;
-//     //     return (-1);
-//     // }
-
-//     // Loop over all the sockets in the set
-//     for(int i = 0; i < FD_SETSIZE; i++){
-//         // Check if the socket is in the set
-//         if(FD_ISSET(i, &copy)){
-//             int sock = i;
-
-//             // If the socket is the server socket, it means there's a new connection
-//             if(sock == server){
-//                 // Accept the new connection
-//                 new_socket = accept(server, (struct sockaddr *)&address, &addrlen);
-
-//                 // Check if accept() returned an error
-//                 if (new_socket < 0) {
-//                     std::cerr << "Accept error: " << strerror(errno) << std::endl;
-//                     return (-1);
-//                 }
-
-//                 // Add the new socket to the master set
-//                 FD_SET(new_socket, &master);
-//             }
-// 			else{
-// 				// Receive data from the socket
-// 				int  r = recv(sock, buff, BUFFER_SIZE, 0);
-// 				// Check if recv() returned an error
-// 				if( r < 0){
-// 					std::cerr << "cant read from the socket" << std::endl;
-// 				}
-// 				buff[r] = '\0';
-// 				// Print the received data
-// 				std::cout << YELLOW << buff << RESET<< std::endl;
-// 				std::cout << "          \033[1;32m 🛠 ...... SERVER ON ...... 🛠\033[0m" << std::endl;
-
-// 				// Parse the request
-// 				Request req(buff);
-// 				req.parseRequest();
-
-// 				std::string path = req.getURL();
-// 				// Loop over all the servers
-// 				for (size_t i = 0; i < servers.size(); i++){
-// 					// If the server's port matches the requested port, send a response
-// 					if (servers[i].port == port){
-// 						sendResponse(sock, req, servers[i]);
-// 					}
-// 				}
-// 			}
-//             // Close the socket
-//             close(sock);
-//             // Remove the socket from the master set
-//             FD_CLR(sock, &master);
-//         }
-//     }
-// }
+					std::size_t found = newClient.request.find("\r\n\r\n");
+					if (found != std::string::npos) {
+						std::cout  << "\n"<<  GREEN <<" ***** Complete request received ***** " << "\n"<<  std::endl;
+					}
+					std::cout << YELLOW << newClient.request << RESET<<std::endl;
+					Request request(newClient.request);
+					request.parseRequest();
+					for(int i = 0; i < servers.size(); i++){
+						if(servers[i].port == port){
+							sendResponse(sock, request, servers[i]);
+						}
+					}
+					// close(sock);
+					// FD_CLR(sock, &master);
+				}
+			}
+		}
+	}
 	
-   while(true){
-    fd_set copy = master; // copy of the master set because select() modifies the set passed to it
-    if (select(FD_SETSIZE, &copy, NULL, NULL, NULL) == -1) {
-        std::cerr << "Select error: " << strerror(errno) << std::endl;
-        return (-1);
-    }
-
-    for(int i = 0; i < FD_SETSIZE; i++){
-        // Check if the socket is in the set
-        if(FD_ISSET(i, &copy)){
-            int sock = i;
-
-            // If the socket is the server socket, it means there's a new connection
-            if(sock == server){
-                // Accept the new connection
-                new_socket = accept(server, (struct sockaddr *)&address, &addrlen);
-
-                // Check if accept() returned an error
-                if (new_socket < 0) {
-                    std::cerr << "Accept error: " << strerror(errno) << std::endl;
-                    return (-1);
-                }
-				  // Make the new socket non-blocking
-                int flags = fcntl(new_socket, F_GETFL, 0);
-                if (flags == -1) {
-                    std::cerr << "Fcntl error: " << strerror(errno) << std::endl;
-                    return (-1);
-                }
-                flags |= O_NONBLOCK;
-                if (fcntl(new_socket, F_SETFL, flags) == -1) {
-                    std::cerr << "Fcntl error: " << strerror(errno) << std::endl;
-                    return (-1);
-                }
-                // Add the new socket to the master set
-                FD_SET(new_socket, &master);
-            }
-            else{
-                // Receive data from the socket
-                int  r = recv(sock, buff, BUFFER_SIZE, 0);
-                // Check if recv() returned an error
-                if( r < 0){
-                    std::cerr << "cant read from the socket" << std::endl;
-                }
-                buff[r] = '\0';
-                // Print the received data
-                std::cout << YELLOW << buff << RESET<< std::endl;
-                // Parse the request
-                Request req(buff);
-                req.parseRequest();
-
-                std::string path = req.getURL();
-                // Loop over all the servers
-                for (size_t i = 0; i < servers.size(); i++){
-                    // If the server's port matches the requested port, send a response
-                    if (servers[i].port == port){
-                        sendResponse(sock, req, servers[i]);
-                    }
-                }
-                // Close the socket
-                close(sock);
-                // Remove the socket from the master set
-                FD_CLR(sock, &master);
-            }
-        }
-    }
-}
-	// close(server);
+	
 }
